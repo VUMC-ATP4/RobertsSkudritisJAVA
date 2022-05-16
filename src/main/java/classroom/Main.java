6dd09ddd13ad5c69fe731cbf1fb0320c115393ca
Roberts Skudritis <r.skudritis@gmail.com>
2022-05-16 21:53:17 +0300

    prepare for homework

--- src/main/java/classroom/Main.java
+++ src/main/java/classroom/Main.java
@@ -1,3 +1,5 @@
+package classroom;
+
 import java.sql.SQLOutput;
 
 public class Main {
---
+++ src/main/java/homework/FirstClass.java
@@ -0,0 +1,11 @@
+package homework;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("This is homework");
+
+
+    }
+
+
+}
